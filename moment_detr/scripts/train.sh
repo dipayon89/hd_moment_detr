847c34eafa1ef763d3d2c5538f5af3314280d435
dhiman paul <dhiman@revesoft.com>
2023-11-02 09:32:40 +0600

    contrastive loss enabled

--- moment_detr/scripts/train.sh
+++ moment_detr/scripts/train.sh
@@ -52,7 +52,4 @@ PYTHONPATH=$PYTHONPATH:. python moment_detr/train.py \
 --results_root ${results_root} \
 --exp_id ${exp_id} \
 --device -1 \
---contrastive_align_loss \
---contrastive_align_loss_coef 1 \
---hidden_dim 512 \
 ${@:1}
