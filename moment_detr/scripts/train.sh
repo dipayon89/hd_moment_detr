e7bf24067e7eb3f78cef0f53ba60e752af50cb02
dhiman paul <dhiman@revesoft.com>
2023-11-03 19:12:24 +0600

    Contrastive loss integrated

--- moment_detr/scripts/train.sh
+++ moment_detr/scripts/train.sh
@@ -52,8 +52,6 @@ PYTHONPATH=$PYTHONPATH:. python moment_detr/train.py \
 --n_epoch ${n_epoch} \
 --results_root ${results_root} \
 --exp_id ${exp_id} \
---device 0 \
---contrastive_align_loss \
---contrastive_align_loss_coef 1 \
+--device -1 \
 --hidden_dim 256 \
 ${@:1}
